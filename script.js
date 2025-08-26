fe5637deb7765feeebd95442d019392bba6e2eb4
AsifAhmedTanjid <asifahmednirob44@gmail.com>
2025-08-27 01:11:13 +0600

    Introducing js and addEventListening to the login button

---
+++ script.js
@@ -0,0 +1,7 @@
+// console.log('script.js connected');
+document.getElementById('login-button').addEventListener('click',function(e){
+    e.preventDefault();
+    // console.log('button clicked');
+    // console.log(e);
+    
+})
