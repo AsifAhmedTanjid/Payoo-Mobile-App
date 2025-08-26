59beca48b220ec218c7ec7de1d7ff455ebc02a19
AsifAhmedTanjid <asifahmednirob44@gmail.com>
2025-08-27 01:24:12 +0600

    login and redirecting to the home page added

--- script.js
+++ script.js
@@ -3,5 +3,18 @@ document.getElementById('login-button').addEventListener('click',function(e){
     e.preventDefault();
     // console.log('button clicked');
     // console.log(e);
+    const mobileNumber= 1812444020;
+    const pinNumber=1234;
+    const mobileNumberValue = parseInt(document.getElementById('mobile-number').value);
+    const pinNumberValue = parseInt(document.getElementById('pin-number').value)
+
+    if(mobileNumber===mobileNumberValue && pinNumber===pinNumberValue){
+        window.location.href ='./home.html'
+    }
+    else
+    {
+        alert('invalid Credentials');
+    }
     
+
 })
